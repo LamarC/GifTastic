9022601f25c1832c05cb0aa4618640c4c02d3fea
LamarC <dviann89@gmail.com>
2018-09-27 00:25:16 -0500

    We have gifs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,137 +1,142 @@
 var gamesArray = ["Earthbound", "Double Dragon", "Fire Emblem", "Punch-Out!!", "Donkey Kong Country", "Super Mario World", "The Legend of Zelda", "Metroid"];
-
-$(document).ready(function(){
- 
-    $(".btn-primary").on("click", function() {
-        
-        var nintendo = $(this).attr("data-game");
+    $('button').on('click', function() {
+        var nintendo = $(this).data('name');
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nintendo + "&api_key=ZKVap5tqWVjtisZqDPeS1CF3MM5obWWs&limit=10";
-// AJAX call
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }) 
-  
-  .done(function(res) {
-    // Log the resulting object & URL
-    console.log(res); //Didn't get anything!!
 
-    var results = res.data;
-
-    for (var i = 0; i < results.length; i++) {
-        
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-        
-        var newGame = $("<div>");
-        
-        var p = $("<p>");
-
-        p.text(results[i].rating);
-        
-        var newGameImage = $("<img>");
-        
-        newGameImage.addClass("anImg");
-        
-        newGameImage.attr("src", results[i].images.fixed_height.url);
-        
-        newGameImage.attr("alt", "nintendo game");
-        
-        newGameImage.attr("data-still", results[i].images.fixed_height_still.url);
-        
-        newGameImage.attr("data-animate", results[i].images.fixed_height.url);
-
-        //attr("data-state", "still");
-        
-        newGame.append(p);
-        
-        newGame.append(newGameImage);
-        
-        newGame.prepend("#gifs");
-    }
-
-}
-
-    $(".anImage").on("click", function() {
-          
-        var state = $(this).attr("data-state"); 
-        console.log(this); 
-
-        if (state == "still") {
-        
-        $(this).attr("src", $(this).data("animate"));
-        
-        $(this).attr("data-state", "animate");
-
-        } else {
-                
-        $(this).attr("src", $(this).data("still"));
-        
-        $(this).attr("data-state", "still");
-        }
-    });  
-  });
-});
-
-//var games = [''];
-
-$("#find-game").on("click", function(){
-    var gameButton = $("#gif-input").val();
-
-    var newButton = $("<button>").addClass("btn-primary").attr("data-name", gameButton).html(gameButton);
-
-    $("gameButton").append(newButton);
-
-    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nintendo + "&api_key=ZKVap5tqWVjtisZqDPeS1CF3MM5obWWs&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
         })
+            .done(function(response) {
+
+
+                console.log(response)
+
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+
+                    var newGame = $("<div>");
+        
+                    var p = $("<p>");
+
+                    p.text(results[i].rating);
+
+                    var newGameImage = $("<img/>"); newGameImage.attr("src", results[i].images.fixed_height.url);
+        
+                    newGameImage.attr("alt", "nintendo game");
+
+                    newGameImage.attr("data-still", results[i].images.fixed_height_still.url);
+
+                    newGameImage.attr("data-animate", results[i].images.fixed_height.url);
+
+//                    attr('data-state', 'still');
+
+                    newGame.append(p);
+
+                    newGame.append(newGameImage);
+
+                    newGame.prependTo($("#gifs")); //It took me forever to find this error
+                }
+
+                $(".anImage").on("click", function () {
+            
+                    var state = $(this).attr("data-state"); 
+                    console.log(this);
+
+                    if (state == "still") {
+        
+                        $(this).attr("src", $(this).data("animate"));
+
+                        $(this).attr("data-state", "animate");
+
+                    } else {
+
+                        $(this).attr("src", $(this).data("still"));
+
+                        $(this).attr("data-state", "still");
+                    }      
+                });
+            });
+    });
+
+    var games = [''];
+
     
-    .done(function(res){
-        var results= res.data;
+        //This function "adds" the buttons 
 
-        for(var i =0; i < results.length; i++) {
-        
-        var newGame = $("<div>");
-        
-        var p = $("<p>");
+        // handles the event when clicked
+        $("#find-game").on("click", function(){
+            var gameButton = $("#gif-input").val();
+            //adds the new animal
 
-        p.text(results[i].rating);
-        
-        var newGameImage = $("<img>");
-        
-        newGameImage.addClass("anImg");
-        
-        newGameImage.attr("src", results[i].images.fixed_height.url);
-        
-        newGameImage.attr("alt", "nintendo game");
-        
-        newGameImage.attr("data-still", results[i].images.fixed_height_still.url);
-        
-        newGameImage.attr("data-animate", results[i].images.fixed_height.url);
+            var newButton = $('<button/>').addClass('btn btn-info animal').attr('data-name',gameButton).html(gameButton);
+            
+            $('.btn-primary').append(newButton);
 
-        //attr("data-state", "still");
-        
-        newGame.append(p);
-        
-        newGame.append(newGameImage);
-        
-        newGame.prepend("#gifs");
-        }
-    
-        $(".anImg").on("click", function(){
-            var state 
+            queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gameButton + "&api_key=ZKVap5tqWVjtisZqDPeS1CF3MM5obWWs&limit=10";
+                console.log(gameButton);
+
+            $.ajax({
+            url: queryURL,
+            method: 'GET'
+            })
+
+            .done(function(res) {
+            
+            $("#gif-input").empty();
+
+            var results = res.data;
+
+                for (var i = 0; i < results.length; i++) {
+
+                    var newGameImage = $('<div/>');
+
+                    var p =$('<p/>');
+
+                    p.text(results[i].rating);
+
+                    var newGameImage = $('<img/>');
+
+                    newGameImage.addClass('anImg')
+
+                    newGameImage.attr('src', results[i].images.fixed_height_still.url);
+
+                    newGameImage.attr('data-still', results[i].images.fixed_height_still.url)
+
+                    newGameImage.attr('data-animate', results[i].images.fixed_height.url)
+
+                    .attr('data-state', 'still');
+
+                    //newGame.append(p);
+
+                    newGame.append(newGameImage);
+
+                    newGame.prependTo($('#gifs'));
+                }
+
+                $('.anImg').on('click', function() {
+            
+                    var state = $(this).attr('data-state'); 
+                    console.log(this);
+
+                    if (state == 'still') {
+                    
+                    $(this).attr('src', $(this).data('animate'));
+                    
+                    $(this).attr('data-state', 'animate');
+
+                    } else {
+                            
+                    $(this).attr('src', $(this).data('still'));
+                    
+                    $(this).attr('data-state', 'still');
+                    }      
+                });
+            });
+
+            $("#gif-input").val("");
+            return false;
         })
-    
-    })
-})
-
-});
-
-
-
-
-
   
-// API key=ZKVap5tqWVjtisZqDPeS1CF3MM5obWWs
+        // API key=ZKVap5tqWVjtisZqDPeS1CF3MM5obWWs
