90799a587c5a84100c038d585fd738b864034962
LamarC <dviann89@gmail.com>
2018-09-26 22:42:10 -0500

    We got info in consolegit add -A

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,48 +6,55 @@ $(document).ready(function(){
         
         var nintendo = $(this).attr("data-game");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nintendo + "&api_key=ZKVap5tqWVjtisZqDPeS1CF3MM5obWWs&limit=10";
-
+// AJAX call
   $.ajax({
     url: queryURL,
     method: "GET"
-  }) //I don't see the error here but the terminal does .. ?
+  }) 
   
-  .then(function(res) {
+  .done(function(res) {
     // Log the resulting object & URL
-    console.log(res);
-    console.log(queryURL);
+    console.log(res); //Didn't get anything!!
 
-    var results = response.data;
+    var results = res.data;
 
     for (var i = 0; i < results.length; i++) {
-        //Making sure we don't get anything crazy here //Rating 
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13")
-        //Creating variables for html tags
+        
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+        
         var newGame = $("<div>");
+        
         var p = $("<p>");
 
         p.text(results[i].rating);
         
         var newGameImage = $("<img>");
+        
+        newGameImage.addClass("anImg");
+        
+        newGameImage.attr("src", results[i].images.fixed_height.url);
+        
+        newGameImage.attr("alt", "nintendo game");
+        
+        newGameImage.attr("data-still", results[i].images.fixed_height_still.url);
+        
+        newGameImage.attr("data-animate", results[i].images.fixed_height.url);
 
-        newGame.attr('src', results[i].images.fixed_height.url);
-        newGame.attr("alt", "nintendo game");
-
-        newGameImage.attr('data-still', results[i].images.fixed_height_still.url)   
-    
-        newGameImage.att('data-animate', results[i].images.fixed_height.url)
-
-        .attr('data-state', 'still');
-
+        //attr("data-state", "still");
+        
         newGame.append(p);
+        
         newGame.append(newGameImage);
+        
         newGame.prepend("#gifs");
     }
 
+}
+
     $(".anImage").on("click", function() {
-            
+          
         var state = $(this).attr("data-state"); 
-        console.log(this);
+        console.log(this); 
 
         if (state == "still") {
         
@@ -65,13 +72,60 @@ $(document).ready(function(){
   });
 });
 
-var games = [''];
+//var games = [''];
 
-function submitButton() {
-    var userInput = $("gif-input").val();
-    searchGif(userInput);
-}
+$("#find-game").on("click", function(){
+    var gameButton = $("#gif-input").val();
 
+    var newButton = $("<button>").addClass("btn-primary").attr("data-name", gameButton).html(gameButton);
+
+    $("gameButton").append(newButton);
+
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nintendo + "&api_key=ZKVap5tqWVjtisZqDPeS1CF3MM5obWWs&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+        })
+    
+    .done(function(res){
+        var results= res.data;
+
+        for(var i =0; i < results.length; i++) {
+        
+        var newGame = $("<div>");
+        
+        var p = $("<p>");
+
+        p.text(results[i].rating);
+        
+        var newGameImage = $("<img>");
+        
+        newGameImage.addClass("anImg");
+        
+        newGameImage.attr("src", results[i].images.fixed_height.url);
+        
+        newGameImage.attr("alt", "nintendo game");
+        
+        newGameImage.attr("data-still", results[i].images.fixed_height_still.url);
+        
+        newGameImage.attr("data-animate", results[i].images.fixed_height.url);
+
+        //attr("data-state", "still");
+        
+        newGame.append(p);
+        
+        newGame.append(newGameImage);
+        
+        newGame.prepend("#gifs");
+        }
+    
+        $(".anImg").on("click", function(){
+            var state 
+        })
+    
+    })
+})
 
 });
 
